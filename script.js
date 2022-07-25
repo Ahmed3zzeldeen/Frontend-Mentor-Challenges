b91f10a653af2ffd0c65458edf91926bb4657270
Ahmed M. Ezzeldeen <73358147+Ahmed3zzeldeen@users.noreply.github.com>
2022-07-25 09:56:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ const projects = [
 	{
 		name: 'qr-code-component-main',
 		github:
-			''
+			'https://github.com/Ahmed3zzeldeen/Frontend-mentor-challenges-master/tree/main/qr-code-component-main'
 	},
 	{
 		name: 'nft-preview-card-component-main',
 		github:
-			''
+			'https://github.com/Ahmed3zzeldeen/Frontend-mentor-challenges-master/tree/main/nft-preview-card-component-main'
 	},
 	{
 		name: 'base-apparel-coming-soon-master',
 		github:
-			''
+			'https://github.com/Ahmed3zzeldeen/Frontend-mentor-challenges-master/tree/main/base-apparel-coming-soon-master'
 	},
 	{
 		name: 'clipboard-landing-page-master',
 		github:
-			''
+			'https://github.com/Ahmed3zzeldeen/Frontend-mentor-challenges-master/tree/main/clipboard-landing-page-master'
 	},
 	{
 		name: 'fylo-landing-page',
 		github:
-			''
+		'https://github.com/Ahmed3zzeldeen/Frontend-mentor-challenges-master/tree/main/fylo-landing-page'
 	},
 	{
 		name: 'huddle-landing-page',
 		github:
-			''
+		'https://github.com/Ahmed3zzeldeen/Frontend-mentor-challenges-master/tree/main/huddle-landing-page'
 	}
 ];
 
@@ -38,12 +38,12 @@ projects.forEach(({ name, github }, i) => {
 
 	listItem.innerHTML = `
 		<a href="/${name}/index.html" target="_blank" >
-			<img src="/${name}/design/desktop-design.jpg" alt="${name}" />
+			<img src="./${name}/design/desktop-design.jpg" alt="${name}" />
 			<p>${i + 1}. ${formatName(name)}</p>
 		</a>
 
 		<div class="links-container">
-			<a href="/${name}/index.html" class="blue" target="_blank" >
+			<a href="./${name}/index.html" class="blue" target="_blank" >
 				<i class="fas fa-eye"></i>
 			</a>
 			<a href="${github}" class="github" target="_blank" >
